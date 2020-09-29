d055125be7d5ebd527c9ed6862136ab9391e0f79
Seth <namdlefhtes@gmail.com>
2020-09-29 00:41:14 -0400

    progress?

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
 // create divs until length is 32. 16 x 16 grid. style divs with css.
 //https://www.theodinproject.com/courses/web-development-101/lessons/etch-a-sketch-project
-//create loop to fill array if this test works.
 
-function generateGrid() {
-    let gridNumber = [0];
-    let i = 0;
-    while (gridNumber.length < 32; i++) {
-        gridNumber.push();
-        break;
-    }
-}; 
-function divFunction () {
-var newDiv = document.createElement("div");
+// do not know why appendchild does not work here. 
+const newDiv = document.createElement('div'); 
 document.body.appendChild(newDiv);
-
-} ;
