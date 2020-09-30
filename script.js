898ac4417519b1373f55b47fb09a2091c49cb935
namdlefhtes <namdlefhtes@gmail.com>
2020-09-30 17:11:20 -0400

    ...

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 // create divs until length is 32. 16 x 16 grid. style divs with css.
 //https://www.theodinproject.com/courses/web-development-101/lessons/etch-a-sketch-project
 
-let numberOfDivs = 0;
+let gridDim = 16;
+
+
+
 
 function createDiv () {
-    let num = 0;
-    let incrementDiv = "div"+ num++;
+ 
     //div held in gridDiv
 let gridDiv = document.createElement("div");
-gridDiv.setAttribute("id", incrementDiv);
+gridDiv.setAttribute("id", "div"+i);
 // text content for div.
 let divContent = document.createTextNode("div test");
     //add gridDiv to divContent
@@ -17,10 +19,8 @@ let divContent = document.createTextNode("div test");
     element.appendChild(gridDiv);
 };
 
-
-//create new div
-function createGrid (){
-    if (numberOfDivs < 32) {
-        createDiv();
-    }
-}
+function buildGrid () {
+    for (let i = 0; i < gridDim * gridDim; i++) {
+    createDiv();
+    
+    };
