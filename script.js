1f3034c4e5561e2022efe039fbdb45a20212a624
namdlefhtes <namdlefhtes@gmail.com>
2020-09-30 14:50:18 -0400

    creates new div

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // create divs until length is 32. 16 x 16 grid. style divs with css.
 //https://www.theodinproject.com/courses/web-development-101/lessons/etch-a-sketch-project
 
-// do not know why appendchild does not work here. 
-const newDiv = document.createElement('div'); 
-document.body.appendChild(newDiv);
+
+//create new div
+function createDiv () {
+    //div held in gridDiv
+   let gridDiv = document.createElement("div");
+   // text content for div.
+   let divContent = document.createTextNode("div test");
+    //add gridDiv to divContent
+    gridDiv.appendChild(divContent);
+     let element = document.getElementById("mainDiv");
+    element.appendChild(gridDiv);
+};
