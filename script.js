0fadcddc4d036fa4c64d7cfa5f902fe0f863bdb2
Seth <namdlefhtes@gmail.com>
2020-10-02 00:05:08 -0400

    next step: make divs into grid

--- script.js
+++ script.js
@@ -2,22 +2,23 @@
 //https://www.theodinproject.com/courses/web-development-101/lessons/etch-a-sketch-project
 
 let gridDim = 16;
+let num = 0;
 
 function createDiv () {
-    let divSuffix = 1;
+    let divSuffix = num++;
     //div held in gridDiv
 let gridDiv = document.createElement("div");
-gridDiv.setAttribute("class", "grid-item");
+gridDiv.setAttribute("class", 'grid-item'+divSuffix);
 // text content for div.
-//let divContent = document.createTextNode("div test");
+let divContent = document.createTextNode("div test "+divSuffix);
     //add gridDiv to divContent
-   // gridDiv.appendChild(divContent);
+    gridDiv.appendChild(divContent);
     let element = document.getElementById("mainDiv");
     element.appendChild(gridDiv);
 };
 
 function buildGrid() {
-    for (let i = 0; i < gridDim + gridDim; i++) {
+    for (let i = 0; i < gridDim * 2; i++) {
     createDiv();
     console.log(i);
     
