02690691b7a989f15d916a815199fa678c25a7e2
Seth <namdlefhtes@gmail.com>
2020-10-08 17:55:46 -0400

    colors

--- script.js
+++ script.js
@@ -3,13 +3,12 @@
 
 let gridDim = 16;
 let num = 0;
+let mouseTarget = document.querySelectorAll(':hover');
 
 //close. not changing colour still.
-function hoverFunction(hover) {
-    //document.getElementById("grid-item").style.backgroundcolor = "red";
-    document.getElementById("grid-item").backgroundColor = "green";
+function hoverFunction() {
+//    mouseTarget.style.color = "red";    
     console.log("mousingoverlog");
-    console.log(hover.target.getElementById);
 
   };
   
@@ -25,7 +24,7 @@ function createDiv () {
     //div held in gridDiv
     let gridDiv = document.createElement("div");
     gridDiv.setAttribute("id", 'grid-item');
-    gridDiv.setAttribute("onmouseover", "hoverFunction()")
+    //gridDiv.setAttribute("onmouseenter", "hoverFunction()")
     gridDiv.style.backgroundColor="white";
     let element = document.getElementsByClassName("grid-container")[0];
     element.appendChild(gridDiv);
