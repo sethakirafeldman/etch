785394f2d7be9b467c8df339d00aa1685581a81e
Seth <namdlefhtes@gmail.com>
2020-10-10 14:19:48 -0400

    MAYBE PROGRESS

--- script.js
+++ script.js
@@ -3,45 +3,49 @@
 
 let gridDim = 16;
 let num = 0;
-let mouseTarget = document.querySelectorAll(':hover');
 
-/*
-function hoverFunction() {
-    //document.getElementsByClassName("grid-item").id=("colorSquare");
-    document.getElementsByClassName("grid-item").onmousover = this.className='permanent-color';
-    console.log("mousingoverlog");
+// !idea to check if square has css property, then change class to make permanent.
+function permanentColor() {
+    const cssElement = document.querySelector('.grid-item:hover');
+    const cssStyle = getComputedStyle(cssElement, "::hover");
+    const cssBackgroundColor = style.backgroundColor;
+    console.log(cssBackgroundColor);
+//        if ( cssStyle = )
 
-  }; */
+};
+//this changes the class of the created div elements, but it gets stuck at the first one.
+/* 
+function permanentColor() 
+    let target = document.querySelector(".grid-item");
+    //target.className="";
+    target.className= "permanent-color";
+    console.log("permcolortest");
+   
+    }; */
+
   
 function buildGrid() {
     for (let i = 0; i < gridDim * 16; i++) {
     createDiv();
     //console.log(i);
     
-    }; 
+    };
 
 function createDiv () {
     let divSuffix = "-" + num++;
     //div held in gridDiv
     let gridDiv = document.createElement("div");
     gridDiv.setAttribute("class", 'grid-item');
-    gridDiv.setAttribute("id", "square" + divSuffix);
-    gridDiv.addEventListener("mouseover", permanentColor, true);
-    //gridDiv.setAttribute("onmouseover", "permanentColor()");
+    gridDiv.setAttribute("id", "square"+divSuffix);
+    //gridDiv.addEventListener("mouseover", permanentColor, true);
+    gridDiv.setAttribute("onmouseover", "permanentColor()");
     gridDiv.style.backgroundColor="white";
     let divClass = document.getElementsByClassName("grid-container")[0];
     divClass.appendChild(gridDiv);
-    
-        };
-
-        //close. not changing colour still.
-function permanentColor() {
-    let gridItem = document.getElementsByClassName("grid-item");
-    gridItem.className="permanent-color";
-    console.log("mousingoverlog");
 };
 
+    };
 
 
- };
-    
+
+     
