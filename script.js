752b681ca309e77b3185f28f4be17111718bd1af
Seth <namdlefhtes@gmail.com>
2020-10-12 13:01:47 -0400

    working. woo.

--- script.js
+++ script.js
@@ -1,45 +1,45 @@
 // create divs until length is 32. 16 x 16 grid. style divs with css.
 //https://www.theodinproject.com/courses/web-development-101/lessons/etch-a-sketch-project
 
-let gridDim = 16;
+let gridDimension = 16;
 let num = 0;
 
-// !idea to check if square has css property, then change class to make permanent.
+//working
+function getElemId(obj) {
+    let selectedSquare = obj;
+    console.log(selectedSquare);
+    selectedSquare.setAttribute("class", "permanent-color");
+
+    };
+/* did not work. prob can delete. 
 function permanentColor() {
+    const green = "rgb(0, 128, 0)";
     const cssElement = document.querySelector('.grid-item:hover');
-    const cssStyle = getComputedStyle(cssElement, "::hover");
-    const cssBackgroundColor = style.backgroundColor;
-    console.log(cssBackgroundColor);
-//        if ( cssStyle = )
-
-};
-//this changes the class of the created div elements, but it gets stuck at the first one.
-/* 
-function permanentColor() 
-    let target = document.querySelector(".grid-item");
-    //target.className="";
-    target.className= "permanent-color";
-    console.log("permcolortest");
-   
-    }; */
-
-  
+    let cssStyle = getComputedStyle(cssElement, "::hover").getPropertyValue("background-color");
+    //const cssBackgroundColor = style.backgroundColor;
+        // if background-color is green, change class.
+        if ( cssStyle = green ) {
+            console.log("square is greenTest");
+            document.getElementById("")
+        };
+}; */
+//works 
 function buildGrid() {
-    for (let i = 0; i < gridDim * 16; i++) {
+    for (let i = 0; i < gridDimension * 16; i++) {
     createDiv();
     //console.log(i);
     
     };
-
+// works
 function createDiv () {
     let divSuffix = "-" + num++;
     //div held in gridDiv
     let gridDiv = document.createElement("div");
     gridDiv.setAttribute("class", 'grid-item');
     gridDiv.setAttribute("id", "square"+divSuffix);
-    //gridDiv.addEventListener("mouseover", permanentColor, true);
-    gridDiv.setAttribute("onmouseover", "permanentColor()");
     gridDiv.style.backgroundColor="white";
+    //gridDiv.setAttribute("onmouseover", "permanentColor()");
+    gridDiv.setAttribute("onmouseenter", "getElemId(this)");
     let divClass = document.getElementsByClassName("grid-container")[0];
     divClass.appendChild(gridDiv);
 };
