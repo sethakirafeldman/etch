677226b9b9a48a7d8dbd2962eabca0e5b392d6b7
Seth <namdlefhtes@gmail.com>
2020-10-17 14:24:06 -0400

    some function

--- script.js
+++ script.js
@@ -5,27 +5,31 @@ let value = "16";
 let gridDimensions = value * value;
 let num = 0
 
+
 // does not do anything yet. 
 function dimPrompt() {
-    let value = prompt("test", "enter dimensions");
+    let value = prompt("enter a number");
+    
     let txt;
         if (value == null || value == "") {
             txt= "no entry";
+
         } else {
-            txt = "You have chosen";
+            txt = "You have chosen " + value + " x " + value ;
             console.log(value);
+            changeGridSize();
         }
         document.getElementById("settings").innerHTML = txt;
     };
+// have to figure out how to pass variables into the css. 
+ function changeGridSize () {
+    value = "";
+    let container = document.getElementById("containerID");
+    container.style.gridTemplateColumns = "repeat(16, 100px)";
+    container.style.gridTemplateRows = "repeat(16, 100px)";  
+ };   
 
-// have this function activate a gridbuilding function.
-    function changeCSS () {
-        document.getElementById("gridID").setAttribute("id", "test")
-        //document.getElementById("gridID").style.gridTemplateColumns = 
-        //document.getElementById("gridID").style.gridTemplateRows =    
-    }; 
-
-//working
+//changes class of selected div to change color.
 function getElemId(obj) {
     let selectedSquare = obj;
     selectedSquare.setAttribute("class", "permanent-color");
@@ -37,21 +41,22 @@ function buildGrid() {
     for (let i = 0; i < gridDimensions; i++) {
     createDiv();
     //console.log(i);
-    
+
     };
-// works
-function createDiv () {
-    let divSuffix = "-" + num++;
-    //div held in gridDiv
-    let gridDiv = document.createElement("div");
-    gridDiv.setAttribute("class", 'grid-item');
-    gridDiv.setAttribute("id", "square"+ divSuffix);
-    gridDiv.style.backgroundColor="white";
-    //gridDiv.setAttribute("onmouseover", "permanentColor()");
-    gridDiv.setAttribute("onmouseenter", "getElemId(this)");
-    let divClass = document.getElementsByClassName("grid-container")[0];
-    divClass.appendChild(gridDiv);
-        };
+
+    // creates div in DOM.
+    function createDiv () {
+        let divSuffix = "-" + num++;
+        //div held in gridDiv
+        let gridDiv = document.createElement("div");
+        gridDiv.setAttribute("class", 'grid-item');
+        gridDiv.setAttribute("id", "square"+ divSuffix);
+        changeGridSize();
+        gridDiv.style.backgroundColor="white";
+        gridDiv.setAttribute("onmouseenter", "getElemId(this)");
+        let divClass = document.getElementsByClassName("grid-container")[0];
+        divClass.appendChild(gridDiv);
+            };
 
     };
 
