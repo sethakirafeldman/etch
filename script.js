1b5fdbe28d917f704bcef9b0caf6939497789358
Seth <namdlefhtes@gmail.com>
2020-10-24 12:32:22 -0400

    buildGrid() now buildDivs().  createDiv moved in

--- script.js
+++ script.js
@@ -15,25 +15,24 @@ function getElemId(obj) {
     selectedSquare.setAttribute("class", "permanent-color");
     };
 
-//works to create divs. not grid
-function buildGrid(value) {
+//works to create divs. called Onload from html.
+function buildDivs() {
     for (let i = 0; i < gridDimensions; i++) {
     createDiv(gridDimensions);
     
-    };
-
-//creates div in DOM.
-    function createDiv () {
-        let divSuffix = "-" + num++;
-        //div held in gridDiv
-        let gridDiv = document.createElement("div");
-        gridDiv.setAttribute("class", 'grid-item');
-        gridDiv.setAttribute("id", "square"+ divSuffix);
-        //changeGridSize();
-        gridDiv.style.backgroundColor="white";
-        gridDiv.setAttribute("onmouseenter", "getElemId(this)");
-        let divClass = document.getElementsByClassName("grid-container")[0];
-        divClass.appendChild(gridDiv);
+    //creates div in DOM.
+        function createDiv () {
+            let divSuffix = "-" + num++;
+            //div held in gridDiv
+            let gridDiv = document.createElement("div");
+            gridDiv.setAttribute("class", 'grid-item');
+            gridDiv.setAttribute("id", "square"+ divSuffix);
+            //changeGridSize();
+            gridDiv.style.backgroundColor="white";
+            gridDiv.setAttribute("onmouseenter", "getElemId(this)");
+            let divClass = document.getElementsByClassName("grid-container")[0];
+            divClass.appendChild(gridDiv);
+                };
             };
 
     };    
@@ -42,7 +41,6 @@ function getDimensions(value) {
     value = window.prompt("Enter a number", "");
         if (value <= 100) { 
         // change default to prompt value
-        //value = window.prompt("test", "");
         changeGridSize(value);
         }
         else {
@@ -52,12 +50,12 @@ function getDimensions(value) {
 
 function changeGridSize(value) {
 
-    cssDimensions = 'repeat(' + value + ', 100px)'; 
-// cssDimensions = 'repeat(32, 100px)';
+    cssDimensions = 'repeat(' + value + ', 25px)';
+    //cssDimensions = 'repeat(' + value + '1fr)'; 
     let container = document.getElementById("containerID");
     container.style.setProperty('grid-template-columns', cssDimensions);
     container.style.setProperty('grid-template-rows', cssDimensions);
-    buildGrid(value + container);
+    buildDivs(value + container);
 };
 
      
